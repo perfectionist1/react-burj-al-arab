7615cbaea46941e9c6e9ea1c187d304572cea852
Md. Mostafizur Rahman <dev.mostafizur@gmail.com>
2020-10-08 18:46:48 +0600

    Initial Front-end, Firebase project, app and configuration completed

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyDmJbURTJffwXWUjLg7S8V5XZQIbjRkOi4",
+    authDomain: "react-burj-al-arab-3f9e0.firebaseapp.com",
+    databaseURL: "https://react-burj-al-arab-3f9e0.firebaseio.com",
+    projectId: "react-burj-al-arab-3f9e0",
+    storageBucket: "react-burj-al-arab-3f9e0.appspot.com",
+    messagingSenderId: "808752379285",
+    appId: "1:808752379285:web:9d13ba9f5c3e5bdc24729c"
+  };
+ 
